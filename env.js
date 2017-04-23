dc2d4df6c1856e2e2cdc4dde1f53be68f2598426
polo-dev <polo.reinhardt@gmail.com>
2017-04-23 03:38:21 +0200

    Create env.js

---
+++ env.js
@@ -0,0 +1,14 @@
+// Account id + password
+var name = 'yourName';
+var password = 'yourPassword';
+
+// Number of fight you want
+var nbFights = 10;
+// do all the fight available
+var allFight = true;
+
+//name of the leaks to fights
+var myLeaks = [
+  "leak1",
+  "leak2"
+];
